29db5cf115287084674834b1cd7fd4e06eab4d1e
Sebastien Klasa <klasa.sebastien@gmail.com>
2023-02-20 22:19:24 +0000

    refactor: remove setup script

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. "$HOME/.cargo/env"
-
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-cargo install --locked trunk
-rustup target add wasm32-unknown-unknown
-cargo install --force cargo-make
